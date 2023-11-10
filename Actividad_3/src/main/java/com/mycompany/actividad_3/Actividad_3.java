7d1d06fab3b697677da4cd2c54556ff1bc6e0b4f
OmarLeal06 <132798139+OmarLeal06@users.noreply.github.com>
2023-11-10 11:46:26 -0700

    Agregando archivos de la actividad 3

---
+++ Actividad_3/src/main/java/com/mycompany/actividad_3/Actividad_3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad_3;
+
+import vista.menu;
+
+/**
+ *
+ * @author omar.leal
+ */
+public class Actividad_3 {
+    
+    
+    public static void main(String[] args) {
+                java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new menu().setVisible(true);
+            }
+        });
+    }
+}
